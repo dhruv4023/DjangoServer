814f44b942fbf0337b4ceb9127e0280b10049f06
dhruv4023 <dhruv20345@gmail.com>
2023-04-22 22:11:10 +0530

    test

--- dserver/settings.py
+++ dserver/settings.py
@@ -81,13 +81,13 @@ WSGI_APPLICATION = 'dserver.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'djongo',
@@ -141,5 +141,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
