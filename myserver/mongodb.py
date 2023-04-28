ce6b06778f926bbc11de73afb83671b350f5f63c
dhruv4023 <dhruv20345@gmail.com>
2023-04-28 22:32:18 +0530

    database changed

--- myserver/mongodb.py
+++ myserver/mongodb.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from django.conf import settings
 
 client = MongoClient(settings.DB_URL)
-db = client["pymongotest"]
+db = client["mysite"]
 project_collection = db["projects"]
 contact_collection = db["contacts"]
 chat_collection = db["chat"]
