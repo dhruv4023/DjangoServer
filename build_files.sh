483ce5efbec7e63d0f8e40988b1aab6da6343880
dhruv <dhruv20345@gmail.com>
2023-05-14 14:56:12 +0530

    try to run

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 
 echo "BUID START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+pip install requirements.txt
+python3.9 manage.py collectstatic
 echo "BUID END" 
