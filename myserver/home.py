272a654ca9875edb868ce259bc87af92bcb07942
dhruv <dhruv20345@gmail.com>
2023-05-14 13:57:45 +0530

    try to run

--- myserver/home.py
+++ myserver/home.py
@@ -6,9 +6,10 @@ from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 
 
+@csrf_exempt
 def index(request):
-    # return render(request, 'index.html')
-    return HttpResponse("Server is running")
+    return render(request, 'index.html')
+    # return HttpResponse("Server is running")
 
 
 @csrf_exempt
